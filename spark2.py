42dcd0db8e9583a95995c9b6f0f11db2159ec55d
Yue <yue_peng@brown.edu>
2018-02-27 21:33:28 -0500

    fastest hw2.py 66s

--- spark2.py
+++ spark2.py
@@ -6,23 +6,29 @@ import os
 import sys
 import time
 import numpy as np
+import re
+try:
+    sc and spark
+except NameError as e:
+    import findspark
+    findspark.init()
 # from operator import add
-from functools import reduce
+# from functools import reduce
 from sklearn.preprocessing import scale
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark import StorageLevel
 # from collections import OrderedDict
 # from pyspark.sql import SQLContext
-from pyspark.mllib.linalg import Vectors
+# from pyspark.mllib.linalg import Vectors
 # from pyspark.mllib.linalg.distributed import RowMatrix
 # from pyspark.sql.types import StructType, StructField
 # from pyspark.sql.types import FloatType
 # from pyspark.sql.functions import log
-from pyspark.mllib.stat import Statistics
-from pyspark.ml.feature import StandardScaler
-from pyspark.sql import DataFrame
-# findspark.init()
+# from pyspark.mllib.stat import Statistics
+# from pyspark.ml.feature import StandardScaler
+# from pyspark.sql import DataFrame
+
 
 
 def authors():
@@ -32,19 +38,15 @@ def authors():
 def main(sc, files):
 
     rdd = sc.wholeTextFiles(",".join(files)).persist(StorageLevel.MEMORY_ONLY_SER)
-    rdd = rdd.map(lambda x: (x[0][34:], x[1])).persist(StorageLevel.MEMORY_ONLY_SER)
-    lines = rdd.mapValues(lambda x: x.rstrip().split("\n")).persist(StorageLevel.MEMORY_ONLY_SER)
+    rdd_id = rdd.map(lambda x: (float(x[0][re.search(r"\d{6}", x[0]).span()[0]:re.search(r"\d{6}", x[0]).span()[1]]), x[1])).persist(StorageLevel.MEMORY_ONLY_SER)
+    lines = rdd_id.mapValues(lambda x: x.rstrip().split("\n")).persist(StorageLevel.MEMORY_ONLY_SER)
     mat = lines.mapValues(lambda x: [*map(str.split, x)]).persist(StorageLevel.MEMORY_ONLY_SER)
     mat_num = mat.mapValues(lambda x: np.array(x).astype(float)).persist(StorageLevel.MEMORY_ONLY_SER)
     scaled_mat = mat_num.mapValues(lambda x: scale(x)).persist(StorageLevel.MEMORY_ONLY_SER)
-    scaled_mat_id = scaled_mat.map(lambda x: (float(x[0][3:9]), x[1])).persist(StorageLevel.MEMORY_ONLY_SER)
-    scaled_mat_labeled = scaled_mat_id.map(lambda x: (("train" if x[0] <= 211316. else "test"), x[1])).persist(StorageLevel.MEMORY_ONLY_SER)
-    
+    train = scaled_mat.filter(lambda x: x[0] <= 211316).persist(StorageLevel.MEMORY_ONLY_SER)
+    test = scaled_mat.filter(lambda x: x[0] > 211316).persist(StorageLevel.MEMORY_ONLY_SER)
 
 
-
-    Xs_train = scaled_mat_id.filter(lambda x: x[0] <= 211316.)
-    Xs_test = scaled_mat_id.filter(lambda x: x[0] > 211316.)
     Xs_train_mat, Xs_test_mat = Xs_train.map(lambda x: ("train", x[1])), Xs_test.map(lambda x: ("test", x[1]))
     XsTrain, XsTest = Xs_train_mat.reduce(lambda x, y: np.vstack((x, y))), Xs_test_mat.reduce(lambda x, y: np.vstack((x, y)))
 
@@ -83,7 +85,8 @@ if __name__ == "__main__":
     if os.path.basename(os.getcwd()) == "brainD15":
         authors()
         start = time.time()
+        files = sorted([s for s in os.listdir(os.getcwd()) if s.endswith(".txt")])
+        main(sc, files)
     else:
         sys.exit("You should move your python script into brainD15 folder.\n")
-    files = sorted([s for s in os.listdir(os.getcwd()) if s.endswith(".txt")])
-    main(sc, files)
+    
