73943bdc20544ea5b7c2165cec60e5a5ed436b89
Yue <yue_peng@brown.edu>
2018-03-07 18:12:57 -0500

    newest scala

--- hw2/build.sbt
+++ hw2/build.sbt
@@ -3,3 +3,36 @@ name := "hw2"
 version := "0.1"
 
 scalaVersion := "2.11.12"
+
+libraryDependencies  ++= Seq(
+  // Last stable release
+  "org.scalanlp" %% "breeze" % "0.13.2",
+
+  // Native libraries are not included by default. add this if you want them (as of 0.7)
+  // Native libraries greatly improve performance, but increase jar sizes.
+  // It also packages various blas implementations, which have licenses that may or may not
+  // be compatible with the Apache License. No GPL code, as best I know.
+  "org.scalanlp" %% "breeze-natives" % "0.13.2",
+
+  // The visualization library is distributed separately as well.
+  // It depends on LGPL code
+  "org.scalanlp" %% "breeze-viz" % "0.13.2"
+)
+
+
+resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
+
+val sparkVersion = "2.3.0"
+
+
+resolvers ++= Seq(
+  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+)
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-hive" % sparkVersion
+)
--- hw2/src/main/scala/Extract.scala
+++ hw2/src/main/scala/Extract.scala
@@ -1,15 +1,28 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import scala.util.matching.Regex
 import java.io.File
+import scala.math
+//import breeze.linalg._
 
 
 object Extract {
   def stringToArray(s: String): Array[String] = s.split(" ")
 
+  def colMeans(arr: Array[Array[Double]]): Array[Double] = arr.transpose.map(_.sum)//arr.map(Vector(_)).reduce(_ + _).toArray
+
+  def scaling(arr: Array[Array[Double]]): Array[Array[Double]] = {
+    val cm = colMeans(arr)
+    val tmp = arr.map(_.zip(cm).map(x => x._1 - x._2))
+    val ss = tmp.map(_.map(math.pow(_, 2)))
+    val stddev = ss.transpose.map(_.sum).map(_/arr.size).map(math.sqrt(_))
+    val mat = arr.map(_.zip(cm).map(x => x._1 - x._2)).map(_.zip(stddev).map(x=>x._1/x._2))
+    mat
+  }
+
   def main(args: Array[String]): Unit = {
     val files_in_dir = new File(System.getProperty("user.dir"))
     val files = files_in_dir.listFiles.map(_.toString)
-    val files_path = files.mkString(",")
+    val files_txt = files.map(_.slice(34, 47)).filter(_.startsWith("sub")).sorted
+    val files_path = files_txt.mkString(",")
     val conf = new SparkConf()
     conf.setAppName("Spark version of HW2")
     conf.setMaster("local[*]")
@@ -17,8 +30,9 @@ object Extract {
     val input = sc.wholeTextFiles(files_path)
     val input2 = input.mapValues(x => x.split("\n"))
     val input3 = input2.mapValues(x => x.map(x => stringToArray(x)))
-    val mat = input3.mapValues(x => x.map(_.map(_.toFloat)))
+    val mat = input3.mapValues(x => x.map(_.map(_.toDouble)))
     val id_pattern = raw"\d{6}".r
-    val mat_float = mat.map(x => (id_pattern.findAllIn(x._1).toList(0).toFloat, x._2))
+    val mat_Double = mat.map(x => (id_pattern.findAllIn(x._1).toSeq.apply(0).toDouble, x._2))
+    val mat_scaled = mat_Double.mapValues(scaling(_))
   }
 }
