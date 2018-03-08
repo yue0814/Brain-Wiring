fc21ae0b0e1ab00dc533dcdf9155b2cd1fbf3acb
Yue <yue_peng@brown.edu>
2018-03-07 22:01:19 -0500

    0307

--- hw2/src/main/scala/Extract.scala
+++ hw2/src/main/scala/Extract.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
 import java.io.File
-import scala.math
+//import scala.math
 //import breeze.linalg._
 
 
@@ -19,20 +20,26 @@ object Extract {
   }
 
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("Spark version of HW2")
+    conf.setMaster("local[*]")
+    conf.set("spark.memory.offHeap.enabled", "true")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
     val files_in_dir = new File(System.getProperty("user.dir"))
     val files = files_in_dir.listFiles.map(_.toString)
     val files_txt = files.map(_.slice(34, 47)).filter(_.startsWith("sub")).sorted
     val files_path = files_txt.mkString(",")
-    val conf = new SparkConf()
-    conf.setAppName("Spark version of HW2")
-    conf.setMaster("local[*]")
-    val sc = new SparkContext(conf)
-    val input = sc.wholeTextFiles(files_path)
-    val input2 = input.mapValues(x => x.split("\n"))
-    val input3 = input2.mapValues(x => x.map(x => stringToArray(x)))
-    val mat = input3.mapValues(x => x.map(_.map(_.toDouble)))
-    val id_pattern = raw"\d{6}".r
-    val mat_Double = mat.map(x => (id_pattern.findAllIn(x._1).toSeq.apply(0).toDouble, x._2))
-    val mat_scaled = mat_Double.mapValues(scaling(_))
+    val input = sc.wholeTextFiles(files_path).persist(StorageLevel.DISK_ONLY)
+    val input2 = input.mapValues(x => x.split("\n")).persist(StorageLevel.DISK_ONLY)
+    val input3 = input2.mapValues(x => x.map(x => stringToArray(x))).persist(StorageLevel.DISK_ONLY)
+    val mat = input3.mapValues(x => x.map(_.map(_.toDouble))).persist(StorageLevel.DISK_ONLY)
+    val id_pattern = "\\d{6}".r
+    val mat_Double = mat.map(x => (id_pattern.findAllIn(x._1).toSeq.apply(0).toDouble, x._2)).persist(StorageLevel.DISK_ONLY)
+    val mat_scaled = mat_Double.mapValues(scaling(_)).persist(StorageLevel.DISK_ONLY)
+    val mat_train = mat_scaled.filter(_._1 <= 211316).map(x => ("train", x._2)).persist(StorageLevel.DISK_ONLY)
+    val mat_test = mat_scaled.filter(_._1 > 211316).map(x => ("test", x._2)).persist(StorageLevel.DISK_ONLY)
+    val XsTrain = mat_train.reduceByKey(_ ++ _).persist(StorageLevel.DISK_ONLY)
+    val XsTest = mat_test.reduceByKey(_ ++ _).persist(StorageLevel.DISK_ONLY)
   }
 }
